afb425274ef9891611e11fe4a131a9874fc35ca1
Daniel Weber <fu@funami.de>
2018-10-11 00:53:13 +0200

    chore(app): Add prettierrc and enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  WEATHER_API_URL: {
+    prefix: 'https://cors-anywhere.herokuapp.com/',
+    base: 'http://samples.openweathermap.org/data/2.5/weather',
+    cityId: '3381303',
+    key: ''
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  WEATHER_API_URL: {
+    prefix: 'https://cors-anywhere.herokuapp.com/',
+    base: 'http://samples.openweathermap.org/data/2.5/weather',
+    cityId: '3381303',
+    key: ''
+  }
 };
 
 /*
