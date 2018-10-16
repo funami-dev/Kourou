aecf33ccadd36e28ee16d5fe647d9c395e99db95
Daniel Weber <fu@funami.de>
2018-10-16 10:27:19 +0200

    feat(weather): Add weather component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './weather.service';
+import { Weatherdata } from '../interfaces/WeatherData';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public weatherData: Weatherdata[] = [];
+
   constructor(private weatherService: WeatherService) {}
 
-  weatherData = {};
-
   getWeatherData(): void {
-    this.weatherService.getWeatherData().subscribe(data => (this.weatherData = { ...data }));
+    this.weatherService.getWeatherData().subscribe(data => {
+      this.weatherData = data;
+    });
   }
 
   ngOnInit() {
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Weatherdata } from '../interfaces/WeatherData';
 import { environment } from '../environments/environment';
 
 @Injectable({
@@ -9,8 +11,9 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
   WEATHER_API_URL = environment.WEATHER_API_URL;
 
-  public getWeatherData() {
+  public getWeatherData(): Observable<any[]> {
     const { prefix, base, cityId, key } = this.WEATHER_API_URL;
-    return this.http.get(`${prefix}${base}?id=${cityId}&appid=${key}`);
+    const _url = `${prefix}${base}?id=${cityId}&appid=${key}`;
+    return this.http.get<Weatherdata[]>(_url);
   }
 }
